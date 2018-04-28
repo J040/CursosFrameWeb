4cb91ce24c17f9836f069a4a84d738595ff5a68d
J040 <umvelhobarbudo@gmail.com>
2018-04-28 15:15:27 -0300

    Rotas, count do mongodb e mais...

--- backend/api/billingCycle/billingCycle.js
+++ backend/api/billingCycle/billingCycle.js
@@ -2,20 +2,20 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true }
+  name: { type: String, required: [true, "Informe o nome do crédito"] },
+  value: { type: Number, min: 0, required: [true, "Informe o valor do crédito"] }
 })
 
 const debtSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  name: { type: String, required: [true, "Informe o nome do débito"] },
+  value: { type: Number, min: 0, required: [true, "Informe o valor do débito"] },
   status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  month: { type: Number, min: 0, max: 12, required: true },
-  year: { type: Number, min: 1970, max: 2100, required: true },
+  name: { type: String, required: [true, "Informe o nome da BillingCycle"] },
+  month: { type: Number, min: [1, "Mês não existente"], max: [12, "Mês não existente"], required: [true, "Informe o mês da BillingCycle"] },
+  year: { type: Number, min: 1970, max: 2100, required: [true, "Informe o ano da BillingCycle"] },
   credits: [creditSchema],
   debts: [debtSchema]
 })
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,7 +6,10 @@ module.exports = function(server) {
   const router = express.Router()
   server.use('/api', router)
 
-  router.route('/teste').get(function(req, res, next){
-    res.send('Funcionou!')
-  })
+  
+
+  //rotas da api
+  const billingCycleService = require('../api/billingCycle/billingCycleService')
+  billingCycleService.register(router, '/billingCycles')
+
 }
