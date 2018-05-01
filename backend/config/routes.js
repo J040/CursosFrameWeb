5a2487aab03cf18a3aa49766b871e6948b7e6d0d
J040 <umvelhobarbudo@gmail.com>
2018-05-01 20:03:37 -0300

    Atualizado de acordo com o curso, porém deu erro e eu não sei onde arruar

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,10 +6,12 @@ module.exports = function(server) {
   const router = express.Router()
   server.use('/api', router)
 
-  
+
 
   //rotas da api
   const billingCycleService = require('../api/billingCycle/billingCycleService')
   billingCycleService.register(router, '/billingCycles')
 
+  const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+  router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
